47ada6cdc042efde0f0d3c3e44d89759de642fa4
919106840328 <70574648+919106840328@users.noreply.github.com>
2020-09-08 16:27:20 +0800

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,73 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QMainWindow>
+#include<QPushButton>
+#include<QLineEdit>
+#include<QHBoxLayout>
+#include<QString>
+#include<QStack>
+#include<QVBoxLayout>
+#include<QGridLayout>
+#include<iterator>
+class calculator : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    calculator(QWidget *parent = 0);
+    ~calculator();
+private:
+    QLineEdit *inputline;
+    QString input="0";
+    bool flat=false;
+    QPushButton *zero;
+    QPushButton *one;
+    QPushButton *two;
+    QPushButton *three;
+    QPushButton *four;
+    QPushButton *five;
+    QPushButton *six;
+    QPushButton *seven;
+    QPushButton *eight;
+    QPushButton *nine;//0-9
+
+    QPushButton *add;
+    QPushButton *sub;
+    QPushButton *div;
+    QPushButton *mul;
+    QPushButton *dec;
+    QPushButton *equ;
+    //+-...
+
+    QPushButton *CE;
+    QPushButton *AC;
+    QPushButton *le;
+    QPushButton *ri;
+
+private slots:
+    void butzero();
+    void butone();
+    void buttwo();
+    void butthree();
+    void butfour();
+    void butfive();
+    void butsix();
+    void butseven();
+    void buteight();
+    void butnine();
+
+    void butadd();
+    void butsub();
+    void butmul();
+    void butdiv();
+    void butdec();
+
+    void butequ();
+    void butlef();
+    void butri();
+    void butCE();
+    void butAC();
+};
+
+#endif // CALCULATOR_H
